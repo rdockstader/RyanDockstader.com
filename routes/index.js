cf66f74d2ff723547793be992d8711e0c8ccd14b
Ryan Dockstader <ryanldockstader@gmail.com>
2019-03-15 22:41:23 -0600

    start fresh

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express     = require("express"),
-    router      = express.Router();
-    
-//Home page
-router.get("/", function(req, res) {
-    res.render("home");
-});
-
-module.exports = router;
